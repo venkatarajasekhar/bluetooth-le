55f705a38bdb4030c67f728b80f5797dd6732784
SVRajasekhar <surevenkatarajasekhar@gmail.com>
2015-08-18 12:35:02 +0530

    uuid.cpp

--- src/btle/uuid.cpp
+++ src/btle/uuid.cpp
@@ -7,10 +7,16 @@
 #include <assert.h>
 
 using namespace btle;
+using namespace std;
 
-namespace {
+namespace UUIDData{
     #define UUID_16BIT_128_POST_FIX "-0000-1000-8000-00805f9b34fb"
+    const  string  UUID_16BIT_128_POST_FIX("-0000-1000-8000-00805f9b34fb");
+    #undef UUID_16BIT_128_POST_FIX
+   
     #define UUID_16_128_FOMAT_LEN 36
+    const unsigned int UUID_16_128_FOMAT_LEN = 36;
+    #define UUID_16_128_FOMAT_LEN
 }
 
 uuid::uuid()
@@ -30,7 +36,7 @@ uuid::uuid(uint16_t uuid16)
 {
 }
 
-uuid::uuid(const std::string& uuid_str)
+uuid::uuid(const string& uuid_str)
 : base(uuid_str),
   uuid16_(0)
 {
@@ -45,8 +51,8 @@ uuid::uuid(const std::string& uuid_str)
         }
         case 4:
         {
-            std::stringstream ss;
-            ss << std::hex << uuid_str;
+            stringstream ss;
+            ss << hex << uuid_str;
             ss >> uuid16_;
             //value_ = "";
             break;
@@ -54,14 +60,14 @@ uuid::uuid(const std::string& uuid_str)
         case 16:
         case 32:
             break;
-        case UUID_16_128_FOMAT_LEN:
+        case UUIDData::UUID_16_128_FOMAT_LEN:
         {
-            std::string::size_type pos(uuid_str.find(UUID_16BIT_128_POST_FIX,
-                                       sizeof(UUID_16BIT_128_POST_FIX)));
-            if( pos != std::string::npos ){
-                std::string uuid_16( uuid_str.substr(pos - 5,4) );
-                std::stringstream ss;
-                ss << std::hex << uuid_16;
+            string::size_type pos(uuid_str.find(UUIDData::UUID_16BIT_128_POST_FIX,
+                                       UUIDData::UUID_16BIT_128_POST_FIX::size()));
+            if( pos != string::npos ){
+                string uuid_16( uuid_str.substr(pos - 5,4) );
+                stringstream ss;
+                ss << hex << uuid_16;
                 ss >> uuid16_;
                 //value_ = "";
             }
@@ -76,7 +82,7 @@ uuid::uuid(const std::string& uuid_str)
 }
 
 uuid::uuid(const uint8_t uuid128[UUID_128_BIN_LENGTH])
-: base(std::string((const char*)uuid128,UUID_128_BIN_LENGTH)),
+: base(string((const char*)uuid128,UUID_128_BIN_LENGTH)),
   uuid16_(0)
 {
 }
@@ -86,7 +92,7 @@ uint16_t uuid::uuid16bit() const
     return uuid16_;
 }
 
-const std::string& uuid::uuid128bit() const
+const string& uuid::uuid128bit() const
 {
     return value_;
 }
@@ -113,12 +119,12 @@ bool uuid::operator == (uint16_t uuid16) const
 
 bool uuid::operator == (const uint8_t uuid128[UUID_128_BIN_LENGTH]) const
 {
-    return value_.compare(std::string((const char*)uuid128,UUID_128_BIN_LENGTH)) != std::string::npos;
+    return value_.compare(string((const char*)uuid128,UUID_128_BIN_LENGTH)) !=string::npos;
 }
 
-bool uuid::operator == (const std::string& uuid_str) const
+bool uuid::operator == (const string& uuid_str) const
 {
-    return value_.compare(uuid_str) != std::string::npos;
+    return value_.compare(uuid_str) != string::npos;
 }
 
 bool uuid::operator != (const uuid& other) const
@@ -136,25 +142,25 @@ bool uuid::operator != (const uint8_t uuid128[UUID_128_BIN_LENGTH]) const
     return !(*this == uuid128);
 }
 
-bool uuid::operator != (const std::string& uuid_str) const
+bool uuid::operator != (const string& uuid_str) const
 {
-    return value_.compare(uuid_str) == std::string::npos;
+    return value_.compare(uuid_str) == string::npos;
 }
 
-std::string uuid::to_string() const
+string uuid::to_string() const
 {
     if( uuid16_ != 0 )
     {
-        std::stringstream ss_hex;
-        ss_hex << std::uppercase
-               << std::hex
-               << std::setw(2)
-               << std::setfill('0')
+        stringstream ss_hex;
+        ss_hex << uppercase
+               << hex
+               << setw(2)
+               << setfill('0')
                << (unsigned int)(unsigned char)((uuid16_&0xFF00)>>8)
-               << std::uppercase
-               << std::hex
-               << std::setw(2)
-               << std::setfill('0')
+               << uppercase
+               << hex
+               << setw(2)
+               << setfill('0')
                << (unsigned int)(unsigned char)(uuid16_);
         
         return ss_hex.str();
